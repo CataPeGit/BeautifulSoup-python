233b3254b6219dda4da9ee95febf16d237b3412f
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2021-09-09 21:50:57 +0300

    Update web_scraping.py

--- Soup pyton/web_scraping.py
+++ Soup pyton/web_scraping.py
@@ -1,5 +1,62 @@
 from bs4 import BeautifulSoup
 import requests 
+import re
+
+search_term = input("What product do you want to search for?") 
+
+url = f"https://www.newegg.ca/p/pl?d={search_term}&N=4131"
+
+page = requests.get(url).text
+
+doc = BeautifulSoup(page, "html.parser")
+
+page_text = doc.find(class_ = "list-tool-pagination-text")
+pages = int(str(page_text).split("/")[-2].split(">")[-1][:1]) # we find the number of pages
+
+items_found = {}
+
+for page in range(1, pages + 1): # we loop trough all the pages
+    url = f"https://www.newegg.ca/p/pl?d={search_term}&N=4131&page={page}"
+    page = requests.get(url).text
+    doc = BeautifulSoup(page, "html.parser")
+
+    div = doc.find(class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
+    #we will filter the results ourselves so we make sure we get the right stuff
+    items = div.find_all(text = re.compile(search_term))
+
+    for item in items:
+        parent = item.parent
+
+        if parent.name != "a":
+            continue
+
+        link = parent['href']
+        next_parent = item.find_parent(class_ = "item-container")
+        try:
+            price = next_parent.find(class_ ="price-current").find("strong").string
+            # we use this dictionary to store price and link
+            items_found[item] = {"price": int(price.replace(",", "")), "link": link}
+        except:
+            pass
+
+# now we will sort the dictionary
+# this means we have to convert it to a list first
+# sort it and then convert back to dictionary
+
+sorted_items = sorted(items_found.items(), key = lambda x: x[1]['price'])
+
+for item in sorted_items:
+    print(item[0]) # the key/name
+    print(f"${items[1]['price']}")
+    print(items[1]['link'])
+    print ("--------------------------")
+    print()
+
+
+
+"""
+from bs4 import BeautifulSoup
+import requests 
 
 url = " HERE GOES THE URL "
 results = requests.get(url).text
@@ -22,7 +79,7 @@ for tr in trs[:10]:
 print(prices)
 
 
-"""
+
 from bs4 import BeautifulSoup
 import re
 
@@ -35,6 +92,7 @@ for tag in tags:
 
 with open("changed.html", "w") as file:
     file.write(str(doc))
+
 
 
 tags = doc.find_all(text = re.compile("\$.*")) #,limit = 1) pt a limita
